19d3247f57f4895611b3e0cf9172b31b950fa0f2
Mustafa Ucuncu <mustafaucuncu@onet.pl>
2020-07-22 06:39:30 +0200

    Split js files

--- lib/voice_chat_web/views/chat_view.ex
+++ lib/voice_chat_web/views/chat_view.ex
@@ -1,3 +1,7 @@
 defmodule VoiceChatWeb.ChatView do
   use VoiceChatWeb, :view
+
+  def render("scripts.html", assigns) do
+    ~E[<script defer src="<%= Routes.static_path(@conn, "/js/chat.js") %>"></script>]
+  end
 end
